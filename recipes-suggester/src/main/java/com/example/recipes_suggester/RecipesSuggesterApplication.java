34e80469b16bb9b24909cca2b8331659b219747b
Daniel Shapira <danielshapira3@gmail.com>
2024-08-05 13:14:49 +0300

    <Fix> Fixed .env file

--- recipes-suggester/src/main/java/com/example/recipes_suggester/RecipesSuggesterApplication.java
+++ recipes-suggester/src/main/java/com/example/recipes_suggester/RecipesSuggesterApplication.java
@@ -2,10 +2,15 @@ package com.example.recipes_suggester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class RecipesSuggesterApplication {
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
+
         SpringApplication.run(RecipesSuggesterApplication.class, args);
     }
 }
---
+++ recipes-suggester/src/main/java/com/example/recipes_suggester/config/EnvConfig.java
@@ -0,0 +1,14 @@
+package com.example.recipes_suggester.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EnvConfig {
+
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure().ignoreIfMissing().load();
+    }
+}
